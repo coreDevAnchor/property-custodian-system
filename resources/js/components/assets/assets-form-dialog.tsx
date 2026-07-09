e96d3f3003786bf03729d13ce20cbb539a629d18
Jerick Ivan Davidon <jerickivand02@gmail.com>
2026-07-09 15:36:06 +0800

    asset view issue and view modal

--- resources/js/components/assets/assets-form-dialog.tsx
+++ resources/js/components/assets/assets-form-dialog.tsx
@@ -119,7 +119,7 @@ export function AssetFormDialog({
 
                 category_id: asset.category.id,
                 location_id: asset.location.id,
-                asset_type_id: asset.assetType.id,
+                asset_type_id: asset.asset_type.id,
 
                 serial_number: asset.serial_number ?? "",
 
