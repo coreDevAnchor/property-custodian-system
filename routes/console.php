68e8e782900fa97cae5fa0b14e4190a879c475e6
Jerick Ivan Davidon <jerickivand02@gmail.com>
2026-08-03 09:10:19 +0800

    Change notification time

--- routes/console.php
+++ routes/console.php
@@ -9,4 +9,5 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command('reminders:send-return')
-    ->dailyAt('08:00');
+    ->dailyAt('09:00')
+    ->timezone('Asia/Manila');
